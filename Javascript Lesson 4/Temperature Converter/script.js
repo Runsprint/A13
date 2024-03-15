b823d5a14bb07a1d9c1790caf5705aba74324073
Runsprint <gvantsakapanadze95@gmail.com>
2024-03-15 09:58:28 +0400

    new task

---
+++ Javascript Lesson 4/Temperature Converter/script.js
@@ -0,0 +1,27 @@
+// როგორ ითვლება, ანუ კონვერტირდება ცელსიუსი ფარენგეიტში :   F = ((9/5)*C ) + 32 
+//ეს ფუნქცია არის ცელსიუსის საკონვერტაციოდ ფარენგეიტში
+
+function celsiusToFahrenheit() {
+    // celsius to fahrenheit 
+    var celsiusInput = document.getElementById("celsius").value
+    var fahrenheitOutput = (celsiusInput * 9/5)+ 32;
+    document.getElementById("fahrenheit").value = fahrenheitOutput.toFixed(2)
+    console.log("Hello Children")
+}
+
+
+
+
+
+
+// როგორ კონვერტირდება ფარენგეიტი ცელსიუსში :                C = 5/9 * (F - 32)
+//დაწერეთ ფუნქცია, ფარენგეიტის საკონვერტაციოდ, ცელსიუსში, ზედა ფორმულის გამოყენებით.
+
+
+function fahrenheitToCelsius() {
+
+// დაწერეთ კოდი
+
+
+
+}
